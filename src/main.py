6904b8c5b65f42b9135e0e9dbef7338b3c3fdeb9
MgilyA <zq6295migly@gmail.com>
2019-02-22 00:25:40 +0900

    ラズパイ共有

--- src/main.py
+++ src/main.py
@@ -171,9 +171,9 @@ def option():
 
 def main():
     while True:
-        #exam_schedule([[8, 10], [12, 20], [21, 30]])
-        #option()
-        #time.sleep(1)
+        exam_schedule([[8, 10], [12, 20], [21, 30]])
+        option()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
