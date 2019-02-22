f91b2b17f4bbfd88348362f91b6bb5d973d5a5cb
MgilyA <zq6295migly@gmail.com>
2019-02-22 23:20:20 +0900

    ラズパイ共有

--- src/main.py
+++ src/main.py
@@ -87,7 +87,7 @@ def main():
                     app.push_msgs(userID, 'ピッタリの料理をご提案します')
                     # カツカレー
                     thumbnail = 'https://img.cpcdn.com/recipes/4939420/m/49983e0dd58f3dc9ee5438c29c7d40be.jpg?u=20307638&p=1518680840'
-                    link = 'https://sites.google.com/view/migly-sample/%E3%83%9B%E3%83%BC%E3%83%A0'
+                    link = 'https://sites.google.com/view/temahabokking0003/%E3%83%9B%E3%83%BC%E3%83%A0'
                     title = 'カツカレー'
                     text = 'とんかつ（チキンでも可） - じゃがいも - 人参 - 玉ねぎ - 福神漬け - オリーブオイル（食材炒め用） - バーモントカレー中辛 - こくまろ甘口 - ゴールデンカレー甘口 - S&Bカレーパウダー - 塩・胡椒 - 醤油 - オイスターソース - かつおダシ'
                     app.push_json(template(thumbnail, link, title, text))
@@ -117,7 +117,7 @@ def main():
                     app.push_msgs(userID, 'ピッタリの料理をご提案します')
                     # 手巻き寿司
                     thumbnail = 'https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/705987.jpeg'
-                    link = 'https://sites.google.com/view/migly-sample/%E3%83%9B%E3%83%BC%E3%83%A0'
+                    link = 'https://sites.google.com/view/temahabokking0002/%E3%83%9B%E3%83%BC%E3%83%A0'
                     title = '手巻き寿司'
                     text = 'イカ - サーモン - いくら - はまち - 鯛 - まぐろ - キュウリ - カイワレ大根 - 大葉'
                     app.push_json(template(thumbnail, link, title, text))
@@ -146,10 +146,10 @@ def main():
                 # case3
                 if work_flag == True and tired_flag == True:
                     app.push_msgs(userID, 'ピッタリの料理をご提案します')
-                    # 手巻き寿司
+                    # すっぽん鍋
                     thumbnail = 'https://imgfp.hotp.jp/IMGH/10/40/P022821040/P022821040_238.jpg'
                     link = 'https://sites.google.com/view/temahabokking0002/%E3%83%9B%E3%83%BC%E3%83%A0'
-                    title = '手巻き寿司'
+                    title = 'すっぽん鍋'
                     text = 'すっぽん様 - はくさい - 豆腐 - 長ネギ - がんもどき、お豆腐 - お好きな野菜 - 水 - 出し昆布 - 酒 - 醤油'
                     app.push_json(template(thumbnail, link, title, text))
 
